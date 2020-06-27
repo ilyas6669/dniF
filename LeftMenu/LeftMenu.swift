eb5eab5f8ddbb5e1f648a4b8d7006df3fd3410a9
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-06-27 08:09:12 +0400

    ..

--- LeftMenu/LeftMenu.swift
+++ LeftMenu/LeftMenu.swift
@@ -62,7 +62,7 @@ extension LeftMenu : UITableViewDataSource,UITableViewDelegate {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return 71
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -111,10 +111,178 @@ extension LeftMenu : UITableViewDataSource,UITableViewDelegate {
         }else if indexPath.row == 14 {
             let cellTerzi : cellTerzi = tableView.dequeueReusableCell(withIdentifier: "cellTerzi") as! cellTerzi
             return cellTerzi
+        }else if indexPath.row == 15 {
+            let cellTerzi2 : cellTerzi2 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi2") as! cellTerzi2
+            return cellTerzi2
+        }else if indexPath.row == 16 {
+            let cellTerzi3 : cellTerzi3 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi3") as! cellTerzi3
+            return cellTerzi3
+        }else if indexPath.row == 17 {
+            let cellTerzi4 : cellTerzi4 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi4") as! cellTerzi4
+            return cellTerzi4
+        }else if indexPath.row == 18 {
+            let cellTerzi5 : cellTerzi5 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi5") as! cellTerzi5
+            return cellTerzi5
+        }else if indexPath.row == 19 {
+            let cellTerzi6 : cellTerzi6 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi6") as! cellTerzi6
+            return cellTerzi6
+        }else if indexPath.row == 20 {
+            let cellTerzi7 : cellTerzi7 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi7") as! cellTerzi7
+            return cellTerzi7
+        }else if indexPath.row == 21 {
+            let cellTerzi8 : cellTerzi8 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi8") as! cellTerzi8
+            return cellTerzi8
+        }else if indexPath.row == 22 {
+            let cellTerzi9 : cellTerzi9 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi9") as! cellTerzi9
+            return cellTerzi9
+        }else if indexPath.row == 23 {
+            let cellTerzi10 : cellTerzi10 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi10") as! cellTerzi10
+            return cellTerzi10
+        }else if indexPath.row == 24 {
+            let cellTerzi11 : cellTerzi11 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi11") as! cellTerzi11
+            return cellTerzi11
+        }else if indexPath.row == 25 {
+            let cellTerzi12 : cellTerzi12 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi12") as! cellTerzi12
+            return cellTerzi12
+        }else if indexPath.row == 26 {
+            let cellTerzi13 : cellTerzi13 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi13") as! cellTerzi13
+            return cellTerzi13
+        }else if indexPath.row == 27 {
+            let cellTerzi14 : cellTerzi14 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi14") as! cellTerzi14
+            return cellTerzi14
+        }else if indexPath.row == 28 {
+            let cellTerzi15 : cellTerzi15 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi15") as! cellTerzi15
+            return cellTerzi15
+        }else if indexPath.row == 29 {
+            let cellTerzi16 : cellTerzi16 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi16") as! cellTerzi16
+            return cellTerzi16
+        }else if indexPath.row == 30 {
+            let cellTerzi17 : cellTerzi17 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi17") as! cellTerzi17
+            return cellTerzi17
+        }else if indexPath.row == 31 {
+            let cellTerzi18 : cellTerzi18 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi18") as! cellTerzi18
+            return cellTerzi18
+        }else if indexPath.row == 32 {
+            let cellTerzi19 : cellTerzi19 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi19") as! cellTerzi19
+            return cellTerzi19
+        }else if indexPath.row == 33 {
+            let cellTerzi20 : cellTerzi20 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi20") as! cellTerzi20
+            return cellTerzi20
+        }else if indexPath.row == 34 {
+            let cellTerzi21 : cellTerzi21 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi21") as! cellTerzi21
+            return cellTerzi21
+        }else if indexPath.row == 35 {
+            let cellTerzi22 : cellTerzi22 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi22") as! cellTerzi22
+            return cellTerzi22
+        }else if indexPath.row == 36 {
+            let cellTerzi23 : cellTerzi23 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi23") as! cellTerzi23
+            return cellTerzi23
+        }else if indexPath.row == 37 {
+            let cellTerzi24 : cellTerzi24 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi24") as! cellTerzi24
+            return cellTerzi24
+        }else if indexPath.row == 38 {
+            let cellTerzi25 : cellTerzi25 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi25") as! cellTerzi25
+            return cellTerzi25
+        }else if indexPath.row == 39 {
+            let cellTerzi26 : cellTerzi26 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi26") as! cellTerzi26
+            return cellTerzi26
+        }else if indexPath.row == 40 {
+            let cellTerzi27 : cellTerzi27 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi27") as! cellTerzi27
+            return cellTerzi27
+        }else if indexPath.row == 41 {
+            let cellTerzi28 : cellTerzi28 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi28") as! cellTerzi28
+            return cellTerzi28
+        }else if indexPath.row == 42 {
+            let cellTerzi29 : cellTerzi29 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi29") as! cellTerzi29
+            return cellTerzi29
+        }else if indexPath.row == 43 {
+            let cellTerzi30 : cellTerzi30 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi30") as! cellTerzi30
+            return cellTerzi30
+        }else if indexPath.row == 44 {
+            let cellTerzi31 : cellTerzi31 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi31") as! cellTerzi31
+            return cellTerzi31
+        }else if indexPath.row == 45 {
+            let cellTerzi32 : cellTerzi32 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi32") as! cellTerzi32
+            return cellTerzi32
+        }else if indexPath.row == 46 {
+            let cellTerzi33 : cellTerzi33 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi33") as! cellTerzi33
+            return cellTerzi33
+        }else if indexPath.row == 47 {
+            let cellTerzi34 : cellTerzi34 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi34") as! cellTerzi34
+            return cellTerzi34
+        }else if indexPath.row == 48 {
+            let cellTerzi35 : cellTerzi35 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi35") as! cellTerzi35
+            return cellTerzi35
+        }else if indexPath.row == 49 {
+            let cellTerzi36 : cellTerzi36 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi36") as! cellTerzi36
+            return cellTerzi36
+        }else if indexPath.row == 50 {
+            let cellTerzi37 : cellTerzi37 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi37") as! cellTerzi37
+            return cellTerzi37
+        }else if indexPath.row == 51 {
+            let cellTerzi38 : cellTerzi38 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi38") as! cellTerzi38
+            return cellTerzi38
+        }else if indexPath.row == 52 {
+            let cellTerzi39 : cellTerzi39 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi39") as! cellTerzi39
+            return cellTerzi39
+        }else if indexPath.row == 53 {
+            let cellTerzi40 : cellTerzi40 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi40") as! cellTerzi40
+            return cellTerzi40
+        }else if indexPath.row == 54 {
+            let cellTerzi41 : cellTerzi41 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi41") as! cellTerzi41
+            return cellTerzi41
+        }else if indexPath.row == 55 {
+            let cellTerzi42 : cellTerzi42 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi42") as! cellTerzi42
+            return cellTerzi42
+        }else if indexPath.row == 56 {
+            let cellTerzi43 : cellTerzi43 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi43") as! cellTerzi43
+            return cellTerzi43
+        }else if indexPath.row == 57 {
+            let cellTerzi44 : cellTerzi44 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi44") as! cellTerzi44
+            return cellTerzi44
+        }else if indexPath.row == 58 {
+            let cellTerzi45 : cellTerzi45 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi45") as! cellTerzi45
+            return cellTerzi45
+        }else if indexPath.row == 60 {
+            let cellTerzi46 : cellTerzi46 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi46") as! cellTerzi46
+            return cellTerzi46
+        }else if indexPath.row == 61 {
+            let cellTerzi47 : cellTerzi47 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi47") as! cellTerzi47
+            return cellTerzi47
+        }else if indexPath.row == 62 {
+            let cellTerzi48 : cellTerzi48 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi48") as! cellTerzi48
+            return cellTerzi48
+        }else if indexPath.row == 63 {
+            let cellTerzi49 : cellTerzi49 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi49") as! cellTerzi49
+            return cellTerzi49
+        }else if indexPath.row == 64 {
+            let cellTerzi50 : cellTerzi50 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi50") as! cellTerzi50
+            return cellTerzi50
+        }else if indexPath.row == 65 {
+            let cellTerzi51 : cellTerzi51 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi51") as! cellTerzi51
+            return cellTerzi51
+        }else if indexPath.row == 66 {
+            let cellTerzi52 : cellTerzi52 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi52") as! cellTerzi52
+            return cellTerzi52
+        }else if indexPath.row == 68 {
+            let cellTerzi53 : cellTerzi53 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi53") as! cellTerzi53
+            return cellTerzi53
+        }else if indexPath.row == 69 {
+            let cellTerzi54 : cellTerzi54 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi54") as! cellTerzi54
+            return cellTerzi54
+        }else if indexPath.row == 70 {
+            let cellTerzi55 : cellTerzi55 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi55") as! cellTerzi55
+            return cellTerzi55
+        }else if indexPath.row == 71 {
+            let cellTerzi56 : cellTerzi56 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi56") as! cellTerzi56
+            return cellTerzi56
+        }else if indexPath.row == 72 {
+            let cellTerzi57 : cellTerzi57 = tableView.dequeueReusableCell(withIdentifier: "cellTerzi57") as! cellTerzi57
+            return cellTerzi57
         }
         
-        let cellHesap : cellHesap = tableView.dequeueReusableCell(withIdentifier: "cellHesap") as! cellHesap
-        return cellHesap
+        let cellTerzi : cellTerzi = tableView.dequeueReusableCell(withIdentifier: "cellTerzi") as! cellTerzi
+        return cellTerzi
         
     }
     
@@ -183,7 +351,7 @@ extension LeftMenu : UITableViewDataSource,UITableViewDelegate {
         }else if indexPath.row == 6 {
             
         }else {
-            var kategoriArrayEng = ["Market","Pharmacy","Hotel","ATM","Post","Barber","Restaurant","Tailor"]
+            var kategoriArrayEng = ["Avukat","Araba Tamir","Araba Parça","Av Malzemesi","Banka","Bankamatik","Belediye","Berber","Benzinlik","Bilgisayar","Bisiklet","Beyaz Eşya","Cafe","Cep Telefonu","Çiçekçi","Çocuk Parkı","Dershane","Düğün","Eczane","Eğlence","Ehliyet","Elektronik","Fabrika","Fırın","Hastane",""]
             
             print("\(kategoriArrayEng[indexPath.row-7])")
             
--- LeftMenu/LeftMenuCell.swift
+++ LeftMenu/LeftMenuCell.swift
@@ -71,6 +71,195 @@ class cellTerzi : UITableViewCell {
     
 }
 
+class cellTerzi2 : UITableViewCell {
+    
+}
+
+class cellTerzi3 : UITableViewCell {
+    
+}
+
+class cellTerzi4 : UITableViewCell {
+    
+}
+
+class cellTerzi5 : UITableViewCell {
+    
+}
+
+class cellTerzi6 : UITableViewCell {
+    
+}
+class cellTerzi7 : UITableViewCell {
+    
+}
+class cellTerzi8 : UITableViewCell {
+    
+}
+
+class cellTerzi9 : UITableViewCell {
+    
+}
+class cellTerzi10 : UITableViewCell {
+    
+}
+class cellTerzi11 : UITableViewCell {
+    
+}
+class cellTerzi12 : UITableViewCell {
+    
+}
+
+class cellTerzi13 : UITableViewCell {
+    
+}
+class cellTerzi14 : UITableViewCell {
+    
+}
+class cellTerzi15 : UITableViewCell {
+    
+}
+class cellTerzi16 : UITableViewCell {
+    
+}
+
+class cellTerzi17 : UITableViewCell {
+    
+}
+
+class cellTerzi18 : UITableViewCell {
+    
+}
+class cellTerzi19 : UITableViewCell {
+    
+}
+class cellTerzi20 : UITableViewCell {
+    
+}
+class cellTerzi21 : UITableViewCell {
+    
+}
+class cellTerzi22 : UITableViewCell {
+    
+}
+class cellTerzi23 : UITableViewCell {
+    
+}
+class cellTerzi24 : UITableViewCell {
+    
+}
+
+
+class cellTerzi25 : UITableViewCell {
+    
+}
+class cellTerzi26 : UITableViewCell {
+    
+}
+class cellTerzi27 : UITableViewCell {
+    
+}
+class cellTerzi28 : UITableViewCell {
+    
+}
+class cellTerzi29 : UITableViewCell {
+    
+}
+class cellTerzi30 : UITableViewCell {
+    
+}
+class cellTerzi31 : UITableViewCell {
+    
+}
+
+class cellTerzi32 : UITableViewCell {
+    
+}
+class cellTerzi33 : UITableViewCell {
+    
+}
+class cellTerzi34 : UITableViewCell {
+    
+}
+class cellTerzi35 : UITableViewCell {
+    
+}
+class cellTerzi36 : UITableViewCell {
+    
+}
+class cellTerzi37 : UITableViewCell {
+    
+}
+class cellTerzi38 : UITableViewCell {
+    
+}
+
+class cellTerzi39 : UITableViewCell {
+    
+}
+class cellTerzi40 : UITableViewCell {
+    
+}
+class cellTerzi41 : UITableViewCell {
+    
+}
+class cellTerzi42 : UITableViewCell {
+    
+}
+class cellTerzi43 : UITableViewCell {
+    
+}
+class cellTerzi44 : UITableViewCell {
+    
+}
+class cellTerzi45 : UITableViewCell {
+    
+}
+
+class cellTerzi46 : UITableViewCell {
+    
+}
+class cellTerzi47 : UITableViewCell {
+    
+}
+class cellTerzi48 : UITableViewCell {
+    
+}
+class cellTerzi49 : UITableViewCell {
+    
+}
+class cellTerzi50 : UITableViewCell {
+    
+}
+class cellTerzi51 : UITableViewCell {
+    
+}
+class cellTerzi52 : UITableViewCell {
+    
+}
+
+class cellTerzi53 : UITableViewCell {
+    
+}
+class cellTerzi54 : UITableViewCell {
+    
+}
+class cellTerzi55 : UITableViewCell {
+    
+}
+class cellTerzi56 : UITableViewCell {
+    
+}
+class cellTerzi57 : UITableViewCell {
+    
+}
+
+
+
+
+
+
+
 
 
 
