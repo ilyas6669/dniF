77e7d0f64cbaa0621f0e30bb285d3a1c0fa751c3
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-07-21 20:04:03 +0400

    first commit

---
+++ HomeController/UnRegisteredHome.swift
@@ -0,0 +1,30 @@
+//
+//  UnRegisteredHome.swift
+//  Fiind
+//
+//  Created by İlyas Abiyev on 7/2/20.
+//  Copyright © 2020 İlyas Abiyev. All rights reserved.
+//
+
+import UIKit
+
+class UnRegisteredHome: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
